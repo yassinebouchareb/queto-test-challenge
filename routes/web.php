4fa92266ffae3b148c43f675f406b9177265035c
yassine bouchare <boucharebyassine@gmail.com>
2024-01-02 10:26:35 +0100

    redirect home to products page

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::redirect('/', '/products');
+
 Route::prefix('products')->group(function () {
     Route::get('/', [ProductController::class, 'index'])->name('products.index');
     Route::get('/create', [ProductController::class, 'create'])->name('products.create');
